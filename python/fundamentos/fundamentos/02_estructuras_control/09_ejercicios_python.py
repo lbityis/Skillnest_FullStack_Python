8a29ad37989fb6b7eb80046b2b3134e9943e193b
danieljmnz09 <elisasoto@liceovvh.cl>
2026-04-14 09:20:48 -0400

    .

--- python/fundamentos/fundamentos/02_estructuras_control/09_ejercicios_python.py
+++ python/fundamentos/fundamentos/02_estructuras_control/09_ejercicios_python.py
@@ -200,18 +200,48 @@ Crea dos arreglos: uno para nombres_productos y otro para precios.
 Permite al usuario ingresar 3 productos con sus precios. Luego, 
 muestra una lista formateada: Producto: [Nombre] - Precio: $[Valor].
 """
+def simulacionInventario():
+    nombres_producto = []
+    precios = []
 
 """14. Generador de Lista de Compras
 Usa un bucle while para que el usuario agregue artículos a una lista de compras. 
 El proceso termina cuando el usuario escribe "terminar". Al final, muestra la lista ordenada alfabéticamente.
 """
+def listaCompra():
+    lista = []
+    while True:
+        item = input("Articulo (o 'terminar')")
+        if item.lower() == "terminar":
+            break
+        lista.append(item)
+    print(f"Ordenada: {sorted(lista)}")
 
 """15. Análisis de Temperaturas
 Solicita las temperaturas de los 7 días de la semana y guárdalas en un arreglo. Muestra:
 El promedio semanal.
 Cuántos días la temperatura fue superior a 25 grados.
 El día con la temperatura más baja (asumiendo que el índice 0 es Lunes).
-# """
+"""
+def analisisTemperatura():
+    dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+    diasSuperior = []
+    total = 0
+    baja = 100
+    diaBaja = ""
+    cant = 0
+    while cant < 7:
+        temps = float(input(f"Ingrese temperatura del dia {dias[cant]}: "))
+        total += temps
+        if temps < baja and temps <25:
+            baja = temps
+            diaBaja = dias[cant]
+        elif temps > 25:
+            diasSuperior.append(dias[cant])
+        cant += 1
+    print(f"El promedio de las temperaturas fue de {total / 7}")
+    print(f"El dia con la temperatura mas baja fue el dia {diaBaja} con {baja}")
+    print(f"Los dias mas calurosos fueron: {diasSuperior}")
 
 # Menu de navegacion para ejercicios
 continuar = True
@@ -269,16 +299,16 @@ while continuar:
         print(filtroArreglo())
     elif opcion == "12":
         print("\nEjecutando ejercicio 12: ")
-        print()
+        print(buscadorCiudad())
     elif opcion == "13":
         print("\nEjecutando ejercicio 13: ")
-        print()
+        print(simulacionInventario())
     elif opcion == "14":
         print("\nEjecutando ejercicio 14: ")
-        print()
+        print(listaCompra())
     elif opcion == "15":
         print("\nEjecutando ejercicio 15: ")
-        print()
+        print(analisisTemperatura())
     elif opcion == "0":
         print("Saliendo...")
         continuar = False
