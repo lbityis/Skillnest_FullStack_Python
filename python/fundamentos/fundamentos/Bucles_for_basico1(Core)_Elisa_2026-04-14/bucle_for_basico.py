edea0f9cfcfff98ec43ad5e53ae6dd28f818eb61
danieljmnz09 <elisasoto@liceovvh.cl>
2026-04-20 10:58:56 -0400

    .

--- python/fundamentos/fundamentos/Bucles_for_basico1(Core)_Elisa_2026-04-14/bucle_for_basico.py
+++ python/fundamentos/fundamentos/Bucles_for_basico1(Core)_Elisa_2026-04-14/bucle_for_basico.py
@@ -5,37 +5,96 @@ usando ejemplos inspirados en videojuegos y situaciones atractivas.
 
 # 1. Generador de niveles
 # Imprime todos los niveles del 0 al 100 (incluyendo el 100).
-# (Tu código aquí)
 
+def Generadorniveles():
+    for nivel in range (0, 101):
+        print(f"Nivel {nivel}")
 
 # 2. Potenciadores de energía (Múltiplos de 2)
 # Imprime los números múltiplos de 2 desde 2 hasta 500 (incluyendo el 500).
-# (Tu código aquí)
 
+def Potenciadorenergia():
+    for i in range (2, 501, 2):
+        print(i)
 
 # 3. Trampa de emojis
 # Recorre los puntos del 1 al 100.
 # - Si el número es divisible por 5, imprime ""
 # - Si es divisible por 10, imprime ""
 # ¡Cuidado con la prioridad en tus condicionales!
-# (Tu código aquí)
 
+def Trampaemojis():
+    for i in range (1, 101):
+        if i % 10 == 0:
+            print("👻") 
+        elif i % 5 == 0:
+            print("🙊")
+        else:
+            print(i)
 
 # 4. Suma colosal
 # Suma todos los números pares del 0 al 500,000 e imprime la suma total.
-# (Tu código aquí)
 
+def Sumacolosal():
+    total = 0
+    for i in range (0, 500001, 2):
+        total = total + i
+    print(f"Resultado final = {total}")
 
 # 5. Retroceso temporal
 # Desde 2024, retrocede de 3 en 3 hasta 0 o menos.
 # Imprime cada valor en la cuenta regresiva.
-# (Tu código aquí)
 
+def Retrocesotemporal():
+    for i in range(2024, -2, -3):
+        print(i)
 
 # 6. Contador dinámico
 # Declara las variables inicio, fin, y salto (por ejemplo: inicio=3, fin=10, salto=2).
 # Imprime los números en el rango que sean múltiplos de 'salto'.
-# (Tu código aquí)
-
 # Ejemplo: si inicio = 3, fin = 10, y salto = 2
 # Se imprimiría: 4, 6, 8, 10
+
+def Contadordinamico():
+    inicio = 1
+    fin = 101
+    salto = 3
+    for i in range (inicio, fin, salto):
+        print(i)
+
+# Menu de navegacion para ejercicios
+continuar = True
+while continuar:
+    print("\n---Ejercicios Python---")
+    print("--- 1.- Ejercicio 1 ---")
+    print("--- 2.- Ejercicio 2 ---")
+    print("--- 3.- Ejercicio 3 ---")
+    print("--- 4.- Ejercicio 4 ---")
+    print("--- 5.- Ejercicio 5 ---")
+    print("--- 6.- Ejercicio 6 ---")
+
+
+    opcion = input("\n----Elige una opcion: (1 - 6) (0 para salir) =")
+    if opcion == "1":
+        print("\nEjecutando ejercicio 1: ")
+        print(Generadorniveles())
+    elif opcion == "2":
+        print("\nEjecutando ejercicio 2: ")
+        print(Potenciadorenergia())
+    elif opcion == "3":
+        print("\nEjecutando ejercicio 3: ")
+        print(Trampaemojis())
+    elif opcion == "4":
+        print("\nEjecutando ejercicio 4: ")
+        print(Sumacolosal())
+    elif opcion == "5":
+        print("\nEjecutando ejercicio 5: ")
+        print(Retrocesotemporal())
+    elif opcion == "6":
+        print("\nEjecutando ejercicio 6: ")
+        print(Contadordinamico())
+    elif opcion == "0":
+        print("Saliendo...")
+        continuar = False
+    else:
+        print("Opcion no valida, intenta otra vez")
