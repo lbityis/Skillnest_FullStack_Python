1c6530618fbc2c6fad976d5826d95eb3b8451574
danieljmnz09 <elisasoto@liceovvh.cl>
2026-09-22 12:15:24 -0300

    .

--- python/flask/fundamentos/19.- conectar a DB/primera_app_mysql/schema.sql
+++ python/flask/fundamentos/19.- conectar a DB/primera_app_mysql/schema.sql
@@ -1,6 +1,16 @@
--- ==========================================================
--- TABLA USUARIOS
--- ==========================================================
+CREATE DATABASE IF NOT EXISTS primera_flask;
+
+USE primera_flask;
+
+CREATE TABLE IF NOT EXISTS mascotas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    tipo VARCHAR(100) NOT NULL,
+    color VARCHAR(100) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
@@ -10,9 +20,13 @@ CREATE TABLE IF NOT EXISTS usuarios (
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- ==========================================================
--- DATOS DE PRUEBA USUARIOS
--- ==========================================================
+INSERT INTO mascotas (nombre, tipo, color) VALUES
+    ("Firulais", "Perro", "Café"),
+    ("Michi", "Gato", "Negro"),
+    ("Luna", "Perro", "Blanco"),
+    ("Nala", "Gato", "Naranjo"),
+    ("Coco", "Conejo", "Blanco");
+
 INSERT INTO usuarios (nombre, email, edad) VALUES
     ("Ana Gómez", "ana@example.com", 28),
     ("Carlos Pérez", "carlos@example.com", 35),
